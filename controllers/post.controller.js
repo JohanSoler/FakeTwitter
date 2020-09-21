e96f27ce51ceefe35a9aba27d66657dbca2a6c9a
unknown <you@example.com>
2020-09-21 17:09:53 -0500

    Terminación del controlador del post

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -1,96 +1,256 @@
-                /**
+/**
                  * TASK:
                  * IMPLEMENT THE CONTROLLER 
                  */
-const dbManager = require ('../database.config/database.manager');
-const operator = require('sequelize');
-const db = require('../database.config/database.manager');
-
-async function createPost (req, res) {
-    
-    // CHECK IF THE REQUEST BODY IS EMPTY
-    if (!req.body) {
-        res.status(400).send({
-          message: "Request body is empty!!!!"
-        });
-        return;
-    }
-    
-    // CREATING THE OBJECT TO PERSIST
-    const newPostObject = {
-        message: req.body.message,
-        published_date: req.body.published_date
-    }
-    
-    // EXECUTING THE CREATE QUERY - INSERT THE OBJECT INTO DATABASE 
-    dbManager.Post.create(newPostObject).then (
-        data => {
-            res.send (data);
-        }
-    ).catch (
-        e => {
-            // Print error on console
-            console.log(e);
-            // Send error message as a response 
-            res.status(500).send({
-                message: "Some error occurred 01"
-            });
-        }
-    );
-}
-
-
-
-
-/**
- * GEt all post
- */
-async function findAllPost (req, res){
-    try {
-        //Execute query
-        const post = await dbManager.Post.findAll({});
-        
-        //Send response
-        res.json({
-                data: post
-        });
-
-    } catch (e) {
-        // Print error on console
-        console.log(e);
-        // Send error message as a response 
-        res.status(500).send({
-            message: "Some error occurred"
-        });
-    }
-}
-
-/**
- * Get post by id
- */
-async function findOnePost (req, res){
-    try {
-        const { idPost } = req.params;
-
-        //Execute query
-        const post = await dbManager.Post.findOne({
-            where: {
-                idPost: idPost
-            }
-        });
-        //Send response
-        res.json(post);
-
-    } catch (e) {
-        // Print error on console
-        console.log(e);
-        // Send error message as a response 
-        res.status(500).send({
-            message: "Some error occurred"
-        });
-    }
-}
-
-exports.createPost = createPost; 
-exports.findAllPost = findAllPost; 
-exports.findOnePost=findOnePost;
+                const dbManager = require ('../database.config/database.manager');
+                const operator = require('sequelize');
+                const db = require('../database.config/database.manager');
+                
+                async function createPost (req, res) {
+                    
+                    // CHECK IF THE REQUEST BODY IS EMPTY
+                    if (!req.body) {
+                        res.status(400).send({
+                          message: "Request body is empty!!!!"
+                        });
+                        return;
+                    }
+                    
+                    // CREATING THE OBJECT TO PERSIST
+                    const newPostObject = {
+                        message: req.body.message,
+                        published_date: req.body.published_date
+                    }
+                    
+                    // EXECUTING THE CREATE QUERY - INSERT THE OBJECT INTO DATABASE 
+                    dbManager.Post.create(newPostObject).then (
+                        data => {
+                            res.send (data);
+                        }
+                    ).catch (
+                        e => {
+                            // Print error on console
+                            console.log(e);
+                            // Send error message as a response 
+                            res.status(500).send({
+                                message: "Some error occurred 01"
+                            });
+                        }
+                    );
+                }
+                
+                
+                
+                
+                /**
+                 * GEt all post
+                 */
+                async function findAllPost (req, res){
+                    try {
+                        //Execute query
+                        const post = await dbManager.Post.findAll({});
+                        
+                        //Send response
+                        res.json({
+                                data: post
+                        });
+                
+                    } catch (e) {
+                        // Print error on console
+                        console.log(e);
+                        // Send error message as a response 
+                        res.status(500).send({
+                            message: "Some error occurred"
+                        });
+                    }
+                }
+                
+                /**
+                 * Get post by id
+                 */
+                async function findOnePost (req, res){
+                    try {
+                        const { idPost } = req.params;
+                
+                        //Execute query
+                        const post = await dbManager.Post.findOne({
+                            where: {
+                                idPost: idPost
+                            }
+                        });
+                        //Send response
+                        res.json(post);
+                
+                    } catch (e) {
+                        // Print error on console
+                        console.log(e);
+                        // Send error message as a response 
+                        res.status(500).send({
+                            message: "Some error occurred"
+                        });
+                    }
+                }
+                
+                /**
+                 * Update post by ID
+                 */
+                async function updatePost (req, res){
+                    /**
+                    * TASK: Completed
+                    * IMPLEMENT THE FUNCTION________- 
+                    */
+                
+                    try{
+                
+                        const { idPost } = req.params;
+                
+                        //search user by id
+                        const post = await dbManager.Post.findOne({
+                            where: {
+                                idPost: idPost
+                            }
+                        });
+                
+                        if(!req.body){
+                            res.status(400).send({
+                                message: "Request body is empty!!!!"
+                              });
+                              return;
+                        }else {
+                            if(req.body.message){
+                                post.message = req.body.message;
+                            }
+                
+                            if(req.body.published_date){
+                                post.published_date = req.body.published_date;
+                            }
+                        }
+                        
+                        post.save().then (
+                            data => {
+                                res.send (data);
+                            }
+                        ).catch (
+                            e => {
+                                console.log(e);
+                                res.status(500).send({
+                                    message: "Some error occurred"
+                                });
+                            }
+                        );
+                
+                    }catch(e){
+                        // Print error on console
+                        console.log(e);
+                        // Send error message as a response 
+                        res.status(500).send({
+                            message: "Some error occurred"
+                        });
+                    }
+                }
+                
+                
+                /**
+                 * Delete an existen post by id
+                 * @param {*} req 
+                 * @param {*} res 
+                 */
+                async function deletePostByID (req, res){ 
+                    /**
+                    * TASK: Completed
+                    * IMPLEMENT THE FUNCTION________- 
+                    */
+                
+                    try{
+                        const { idPost } = req.params;
+                
+                        //search user by id
+                        const post = await dbManager.Post.findOne({
+                            where: {
+                                idPost: idPost
+                            }
+                        });
+                
+                        await post.destroy()
+                
+                        res.send({
+                            message: post.message+" has been deleted successfully"
+                        });
+                
+                
+                    }catch(e){
+                        // Print error on console
+                        console.log(e);
+                        // Send error message as a response 
+                        res.status(500).send({
+                            message: "Some error occurred"
+                        });
+                    }
+                
+                }
+                
+                /**
+                 * 
+                 * @param {*} req 
+                 * @param {*} res 
+                 */
+                async function deleteAllPost (req, res){
+                    /**
+                    * TASK: Completed
+                    * IMPLEMENT THE FUNCTION________- 
+                    */
+                
+                    try{
+                        
+                        try{
+                            while(dbManager.Post.findOne()!=null){
+                                const postDelete = await dbManager.Post.findOne();
+                                await postDelete.destroy();
+                            }
+                        }catch{
+                            res.send({
+                                message: "all posts has been deleted successfully"
+                            });
+                        }
+                    }catch(e){
+                        // Print error on console
+                        console.log(e);
+                        // Send error message as a response 
+                        res.status(500).send({
+                            message: "Some error occurred"
+                        });
+                    }}
+                
+                
+                async function findAllPostByCreatedDate (req, res){
+                    try {
+                        const { date } = req.params;
+                
+                        //Execute query
+                        const post = await dbManager.Post.findOne({
+                            where: {
+                                published_date: date
+                            }
+                        });
+                        //Send response
+                        res.json(post);
+                
+                    } catch (e) {
+                        // Print error on console
+                        console.log(e);
+                        // Send error message as a response 
+                        res.status(500).send({
+                            message: "Some error occurred"
+                        });
+                    }
+                }
+                
+                
+                
+                exports.createPost = createPost; 
+                exports.findAllPost = findAllPost; 
+                exports.findOnePost=findOnePost;
+                exports.updatePost=updatePost;
+                exports.deletePostByID=deletePostByID;
+                exports.deleteAllPost=deleteAllPost;
+                exports.findAllPostByCreatedDate=findAllPostByCreatedDate;
--- routes/posts.route.js
+++ routes/posts.route.js
@@ -4,23 +4,43 @@ const postController = require ('../controllers/post.controller');
 
 /**
  * TASK:
- * CREATE THE ROUTES _________________________________________________________ 
+ * CREATE THE ROUTES ___________________ 
  */
   
 /**
- * GET Route to list all users
+ * GET Route to list all post
  */
 router.get('/', postController.findAllPost);
 
 /**
- * GET Route to find user by id
+ * GET Route to find post by id
  */
 router.get('/:idPost', postController.findOnePost);
 
 /**
- * POST Route to create user
+ * POST Route to create post
  */
 router.post ('/',postController.createPost);
 
+/**
+ * PUT Route to update an post by id
+ */
+router.put ('/:idPost',postController.updatePost);
+
+/**
+ * DELETE Route to delete an post by id
+ */
+router.delete ('/:idPost',postController.deletePostByID);
+
+/**
+ * DELETE Route to delete all post
+ */
+router.delete ('/',postController.deleteAllPost);
+
+/**
+ * TASK:
+ * ADD THE MISSING ROUTES __________________ 
+ */
+router.get('/date/:date', postController.findAllPostByCreatedDate);
+
 module.exports = router;
-  

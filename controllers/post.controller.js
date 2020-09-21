0cb03512d24277737439a6798057a77706e87438
FreddyPatarroyo <frpatarroyo@poligran.edu.co>
2020-09-21 15:40:13 -0500

    Crear post y get de el esquema posts

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -2,3 +2,65 @@
                  * TASK:
                  * IMPLEMENT THE CONTROLLER 
                  */
+const dbManager = require ('../database.config/database.manager');
+const operator = require('sequelize');
+const db = require('../database.config/database.manager');
+
+async function createPost (req, res) {
+    
+    // CHECK IF THE REQUEST BODY IS EMPTY
+    if (!req.body) {
+        res.status(400).send({
+          message: "Request body is empty!!!!"
+        });
+        return;
+    }
+    
+    // CREATING THE OBJECT TO PERSIST
+    const newPostObject = {
+        message: req.body.message,
+        published_date: req.body.published_date
+    }
+    
+    // EXECUTING THE CREATE QUERY - INSERT THE OBJECT INTO DATABASE 
+    dbManager.Post.create(newPostObject).then (
+        data => {
+            res.send (data);
+        }
+    ).catch (
+        e => {
+            // Print error on console
+            console.log(e);
+            // Send error message as a response 
+            res.status(500).send({
+                message: "Some error occurred 01"
+            });
+        }
+    );
+}
+
+/**
+ * GEt all post
+ */
+async function findAllPost (req, res){
+    try {
+        //Execute query
+        const post = await dbManager.Post.findAll({});
+        
+        //Send response
+        res.json({
+                data: post
+        });
+
+    } catch (e) {
+        // Print error on console
+        console.log(e);
+        // Send error message as a response 
+        res.status(500).send({
+            message: "Some error occurred"
+        });
+    }
+}
+
+exports.createPost = createPost; 
+exports.findAllPost = findAllPost; 
--- routes/posts.route.js
+++ routes/posts.route.js
@@ -1,11 +1,21 @@
 var express = require('express');
 var router = express.Router();
+const postController = require ('../controllers/post.controller'); 
 
 /**
  * TASK:
  * CREATE THE ROUTES _________________________________________________________ 
  */
   
+/**
+ * GET Route to list all users
+ */
+router.get('/', postController.findAllPost);
+
+/**
+ * POST Route to create user
+ */
+router.post ('/',postController.createPost);
 
 module.exports = router;
   
